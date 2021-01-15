9b29e7618eb2c76da350f94954a803fbaeb10799
Carl Hinderer <carl.hinderer4@gmail.com>
2021-01-15 11:39:29 -0700

    Added seeder for invoices and packages.

--- lojistic/db/seeds.rb
+++ lojistic/db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+NUMBER_OF_INVOICES = 20
+
+def create_fake_invoices()
+  NUMBER_OF_INVOICES.times do
+    invoice_date = Date.today - rand(1..30)
+    invoice_type = Invoice::INVOICE_TYPES.sample
+    amount = rand(1..100)
+    reference_number = Faker::Barcode::isbn
+
+    Invoice.create(invoice_type: invoice_type,
+                   date: invoice_date, 
+                   amount: amount,
+                   reference_number: reference_number)
+  end
+end
+
+def create_fake_packages()
+  Invoice.all.each do |i|
+    num_packages = rand(1..10)
+    num_packages.times do
+      tracking_number = Faker::Barcode::isbn
+      service_name = Package::SERVICE_NAMES.sample
+
+      package = Package.new(tracking_number: tracking_number,
+                            service_name: service_name,
+                            ship_date: i.date,
+                            expected_delivery_date: i.date + rand(1..5))
+      i.packages << package
+    end
+  end
+end
+
+create_fake_invoices
+create_fake_packages
