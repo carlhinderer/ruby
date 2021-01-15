7e8fb01b837d7ae0044bd8899b9424c67f0c5386
Carl Hinderer <carl.hinderer4@gmail.com>
2021-01-15 11:10:39 -0700

    Wired up creation of new reports.

--- lojistic/app/controllers/reports_controller.rb
+++ lojistic/app/controllers/reports_controller.rb
@@ -3,10 +3,34 @@ class ReportsController < ApplicationController
     @reports = Report.where(:deleted => :false).all
   end
 
+  def show
+    @report = Report.find params[:id]
+  end
+
+  def new
+    @report = Report.new
+  end
+
+  def create
+    @report = Report.new(report_params)
+    if @report.save
+      flash[:notice] = "Report was successfully created. Currently being generated."
+      redirect_to reports_path
+    else
+      render 'new'
+    end
+  end
+
   def destroy
     @report = Report.find params[:id]
     @report.soft_delete
     flash[:notice] = "Report was successfully deleted."
     redirect_to reports_path
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:report_type, :start_date, :end_date)
   end
 end
--- lojistic/app/models/report.rb
+++ lojistic/app/models/report.rb
@@ -1,7 +1,8 @@
 class Report < ApplicationRecord
+  attribute :deleted, default: :false
   REPORT_TYPES = ['InvoiceSummary']
 
-  validates :report_type, :start_date, :end_date, :deleted, presence: true
+  validates :report_type, :start_date, :end_date, presence: true
   validates :report_type, inclusion: REPORT_TYPES
   
   def soft_delete()
