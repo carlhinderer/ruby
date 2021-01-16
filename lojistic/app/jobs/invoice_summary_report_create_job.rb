7ad50d5ecbb7f72974ed98f9f558783876e3a7f1
Carl Hinderer <carl.hinderer4@gmail.com>
2021-01-15 23:31:07 -0700

    Now attaching csv file to Report record.

--- lojistic/app/jobs/invoice_summary_report_create_job.rb
+++ lojistic/app/jobs/invoice_summary_report_create_job.rb
@@ -3,23 +3,52 @@ require 'csv'
 class InvoiceSummaryReportCreateJob < ApplicationJob
   queue_as :default
 
-  CSV_HEADERS = ['InvoiceType', 'Date', 'Amount', 'ReferenceNumber', 
-    'NumberOfPackages', 'UniqueServices']
+  CSV_HEADERS = ['InvoiceType', 
+                 'Date', 
+                 'Amount', 
+                 'ReferenceNumber', 
+                 'NumberOfPackages', 
+                 'UniqueServices']
+
+  PATH = Rails.root.join('storage')
 
   def perform(report)
-    path = Rails.root.join('storage')
-    filename = "InvoiceSummary-#{report.start_date}-#{report.end_date}.csv"
-    CSV.open(path.join(filename), 'wb') do |csv|
+    create_csv(report)
+    attach_csv_to_report(report)
+  end
+
+  def create_csv(report)
+    CSV.open(filepath(report), 'wb') do |csv|
       csv << CSV_HEADERS
       report.invoice_list.each do |i|
-        row = [i.invoice_type, 
-               i.date, 
-               i.amount, 
-               i.reference_number, 
-               i.packages.count,
-               i.num_unique_services]
-        csv << row
+        csv << csv_row(i)
       end
     end
+  end
+
+  def attach_csv_to_report(report)
+    report.csv_report.attach(
+      io: File.open(filepath(report)),
+      filename: filename(report),
+      content_type: 'text/csv')
+  end
+
+  def filepath(report)
+    PATH.join(filename(report))
+  end
+
+  def filename(report)
+    "InvoiceSummary-#{report.start_date}-#{report.end_date}.csv"
+  end
+
+  def csv_row(invoice)
+    [
+      invoice.invoice_type,
+      invoice.date, 
+      invoice.amount,
+      invoice.reference_number,
+      invoice.packages.count,
+      invoice.num_unique_services
+    ]
   end
 end
