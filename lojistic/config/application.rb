4f60fd4cd83c08b834ae9f7e78bfb075711efb5a
Carl Hinderer <carl.hinderer4@gmail.com>
2021-01-17 21:41:22 -0700

    Added model specs.

--- lojistic/config/application.rb
+++ lojistic/config/application.rb
@@ -8,16 +8,16 @@ module Lojistic
   class Application < Rails::Application
     config.load_defaults 5.2
     config.active_job.queue_adapter = :delayed_job
-  end
-end
 
-config.generators do |g|
-  g.test_framework :rspec,
-    :fixtures => true,
-    :view_specs => false,
-    :helper_specs => false,
-    :routing_specs => false,
-    :controller_specs => true,
-    :request_specs => true
-  g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    config.generators do |g|
+      g.test_framework :rspec,
+        :fixtures => true,
+        :view_specs => false,
+        :helper_specs => false,
+        :routing_specs => false,
+        :controller_specs => true,
+        :request_specs => true
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
+  end
 end
---
+++ lojistic/spec/models/report_spec.rb
@@ -0,0 +1,7 @@
+describe Report, type: :model do
+  let(:report) { build(:report) }
+  
+  it "should be true" do
+    expect(nil).to be_nil
+  end
+end
